7d06702c92dedf201cf43d801e05b3023307d3f1
Georgios Merezas <meg@in.tum.de>
2022-06-30 21:13:02 +0200

    hopefully the last fix

--- Implementierung/salsa_crypt/salsa_crypt_v1.c
+++ Implementierung/salsa_crypt/salsa_crypt_v1.c
@@ -4,7 +4,7 @@
 
 #include "../salsa_core/salsa_core_v1.h"
 
-void salsa20_crypt_v1(size_t mlen, const uint8_t msg[mlen], uint8_t cipher[mlen], uint32_t key[8], uint64_t iv)
+void salsa_crypt_v1(size_t mlen, const uint8_t msg[mlen], uint8_t cipher[mlen], uint32_t key[8], uint64_t iv)
 {
     uint32_t in[16];
 
