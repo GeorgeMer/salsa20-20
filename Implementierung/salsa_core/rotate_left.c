8262580b3dd35e5c31f5325ae63454aee28dfcd9
Fabian Degen <ge47leq@mytum.de>
2022-07-01 20:58:45 +0200

    rotate_left own header and c file in core folder

---
+++ Implementierung/salsa_core/rotate_left.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+#include "rotate_left.h"
+
+uint32_t rotateLeft(uint32_t number, uint32_t amount)
+{
+    return (number << amount) | (number >> (32 - amount));
+}
---
+++ Implementierung/salsa_core/rotate_left.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+uint32_t rotateLeft(uint32_t number, uint32_t amount);
