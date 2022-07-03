9e1444987e8582aa43b282cd58160ae8ff2c7c00
Fabian Degen <ge47leq@mytum.de>
2022-07-03 20:14:24 +0200

    removed boilerplate code

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -1,12 +1,7 @@
 #define _POSIX_C_SOURCE 199309L
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <unistd.h>
 #include <getopt.h>
 #include <errno.h>
 #include <stdbool.h>
-#include <sys/stat.h>
 #include <string.h>
 #include <time.h>
 #include <inttypes.h>
@@ -15,7 +10,7 @@
 #include "salsa_crypt/salsa_crypt_v1.h"
 #include "salsa_crypt/salsa_crypt_v2.h"
 
-#include "file_IO"
+#include "file_IO.h"
 
 static struct option long_options[] =
     {
@@ -50,57 +45,6 @@ void print_help(const char *progname)
 {
     print_usage(progname);
     fprintf(stderr, "\n%s", help_msg);
-}
-
-const uint8_t *read_file(const char *path)
-{
-    FILE *file = fopen(path, "r");
-    char *message = NULL;
-
-    // error handling
-    if (file == NULL)
-    {
-        perror("Error opening file ");
-        exit(EXIT_FAILURE);
-    }
-
-    struct stat statbuf;
-    if (fstat(fileno(file), &statbuf))
-    {
-        perror("Error retrieving file stats ");
-        goto error;
-    }
-
-    if (!S_ISREG(statbuf.st_mode) || statbuf.st_size <= 0)
-    {
-        fprintf(stderr, "Error processing file: Not a regular file or invalid size\n");
-        goto error;
-    }
-
-    if (!(message = malloc(statbuf.st_size + 1)))
-    {
-        fprintf(stderr, "Error reading file: Could not allocate enough memory\n");
-        goto error;
-    }
-
-    if (!fread(message, 1, statbuf.st_size, file))
-    {
-        perror("Error reading file ");
-        free(message);
-        goto error;
-    }
-
-    // close successfully read file, add 0 byte add the end of char array and convert it to uint8_t array
-    fclose(file);
-    message[statbuf.st_size] = '\0';
-    mlen = statbuf.st_size;
-    return (const uint8_t *)message;
-
-    // label to jump to if error occured reading input file
-    // closes file and ends the program by returning EXIT_FAILURE
-error:
-    fclose(file);
-    exit(EXIT_FAILURE);
 }
 
 uint64_t convert_string_to_uint64_t(const char *string)
