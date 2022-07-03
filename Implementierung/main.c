29eb76b241a8e764a18ed6e125d12408c12763a1
Fabian Degen <ge47leq@mytum.de>
2022-07-03 20:18:42 +0200

    moved number_conversions to own folder

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -47,62 +47,6 @@ void print_help(const char *progname)
     fprintf(stderr, "\n%s", help_msg);
 }
 
-uint64_t convert_string_to_uint64_t(const char *string)
-{
-    if (*string == '-')
-    {
-        fprintf(stderr, "Negative numbers are not allowed as options.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    char *endptr;
-    errno = 0;
-    uint64_t result = strtoull(string, &endptr, 0);
-
-    // error handling
-    if (endptr == string || *endptr != '\0')
-    {
-        fprintf(stderr, "%s could not be converted to uint64_t\n", string);
-        exit(EXIT_FAILURE);
-    }
-    else if (errno == ERANGE)
-    {
-        fprintf(stderr, "%s over - or underflows uint64_t\n", string);
-        exit(EXIT_FAILURE);
-    }
-    else if (errno == EINVAL)
-    {
-        fprintf(stderr, "%s No conversion could be performed from String to uint64_t\n", string);
-        exit(EXIT_FAILURE);
-    }
-    return result;
-}
-
-uint32_t convert_string_to_uint32_t(const char *string, int base)
-{
-    char *endptr;
-    errno = 0;
-    uint32_t result = strtoul(string, &endptr, base);
-
-    // error handling
-    if (endptr == string || *endptr != '\0')
-    {
-        fprintf(stderr, "%s could not be converted to uint32_t\n", string);
-        exit(EXIT_FAILURE);
-    }
-    else if (errno == ERANGE)
-    {
-        fprintf(stderr, "%s over - or underflows uint32_t\n", string);
-        exit(EXIT_FAILURE);
-    }
-    else if (errno == EINVAL)
-    {
-        fprintf(stderr, "%s No conversion could be performed from String to uint32_t\n", string);
-        exit(EXIT_FAILURE);
-    }
-    return result;
-}
-
 uint64_t gettime_in_seconds(const struct timespec start, const struct timespec end)
 {
     return ((uint64_t)(end.tv_sec - start.tv_sec)) + ((uint64_t)((end.tv_nsec - start.tv_nsec) / 1000000000));
