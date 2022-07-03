e7ce36ffd73e01890b2535494857b96a78da2ec5
David Csida <david.csida@tum.de>
2022-07-03 17:37:33 +0200

    changed quarterround function so that it takes pointers instead of arrays as inputs

--- Implementierung/salsa_core/salsa_core_v1.c
+++ Implementierung/salsa_core/salsa_core_v1.c
@@ -25,12 +25,14 @@ void bytesToInt(uint32_t *p, uint8_t *bytes)
     uint32_t out = bytes[0] + (bytes[1] << 8) + (bytes[2] << 16) + (bytes[3] << 24);
 }
 
-void quarterRound(uint32_t output[4], const uint32_t input[4])
+/* void quarterRound(uint32_t output[4], const uint32_t input[4]) */
+void quarterRound(uint32_t *out0, uint32_t *out1, uint32_t *out2, uint32_t *out3,
+                  uint32_t in0, uint32_t in1, uint32_t in2, uint32_t in3)
 {
-    output[1] = input[1] ^ (rotateLeft(input[0] + input[3], 7));
-    output[2] = input[2] ^ (rotateLeft(input[1] + input[0], 9));
-    output[3] = input[3] ^ (rotateLeft(input[2] + input[1], 13));
-    output[0] = input[0] ^ (rotateLeft(input[3] + input[2], 18));
+    *out1 = in1 ^ (rotateLeft(in0 + in3, 7));
+    *out2 = in2 ^ (rotateLeft(in1 + in0, 9));
+    *out3 = in3 ^ (rotateLeft(in2 + in2, 13));
+    *out0 = in0 ^ (rotateLeft(in3 + in3, 18));
 }
 
 void rowRound(uint32_t output[16], const uint32_t input[16])
@@ -38,45 +40,17 @@ void rowRound(uint32_t output[16], const uint32_t input[16])
     uint32_t temp_in[4];
     uint32_t temp_out[4];
 
-    temp_in[0] = input[0];
-    temp_in[1] = input[1];
-    temp_in[2] = input[2];
-    temp_in[3] = input[3];
-    quarterRound(temp_out, temp_in);
-    output[0] = temp_out[0];
-    output[1] = temp_out[1];
-    output[2] = temp_out[2];
-    output[3] = temp_out[3];
+    quarterRound(&output[0], &output[1], &output[2], &output[3],
+                 input[0], input[1], input[2], input[3]);
 
-    temp_in[0] = input[5];
-    temp_in[1] = input[6];
-    temp_in[2] = input[7];
-    temp_in[3] = input[4];
-    quarterRound(temp_out, temp_in);
-    output[5] = temp_out[0];
-    output[6] = temp_out[1];
-    output[7] = temp_out[2];
-    output[4] = temp_out[3];
+    quarterRound(&output[5], &output[6], &output[7], &output[4],
+                 input[5], input[6], input[7], input[4]);
 
-    temp_in[0] = input[10];
-    temp_in[1] = input[11];
-    temp_in[2] = input[8];
-    temp_in[3] = input[9];
-    quarterRound(temp_out, temp_in);
-    output[10] = temp_out[0];
-    output[11] = temp_out[1];
-    output[8] = temp_out[2];
-    output[9] = temp_out[3];
+    quarterRound(&output[10], &output[11], &output[8], &output[9],
+                 input[10], input[11], input[8], input[9]);
 
-    temp_in[0] = input[15];
-    temp_in[1] = input[12];
-    temp_in[2] = input[13];
-    temp_in[3] = input[14];
-    quarterRound(temp_out, temp_in);
-    output[15] = temp_out[0];
-    output[12] = temp_out[1];
-    output[13] = temp_out[2];
-    output[14] = temp_out[3];
+    quarterRound(&output[15], &output[12], &output[13], &output[14],
+                 input[15], input[12], input[13], input[14]);
 }
 
 void columnRound(uint32_t output[16], const uint32_t input[16])
@@ -141,7 +115,7 @@ uint32_t littleendian(uint8_t input[4])
            input[3] * 16777216;
 }
 
-uint8_t salsa20_hash(uint8_t output[64], const uint8_t input[64])
+void salsa20_hash(uint8_t output[64], const uint8_t input[64])
 {
     uint8_t temp_input[4];
     uint32_t x[16];
@@ -178,7 +152,7 @@ uint8_t salsa20_hash(uint8_t output[64], const uint8_t input[64])
     }
 }
 
-uint8_t salsa20_expansion(uint8_t output[64], uint8_t k[32], int8_t n[16])
+void salsa20_expansion(uint8_t output[64], uint8_t k[32], int8_t n[16])
 {
     uint8_t temp_input[64];
 
@@ -189,14 +163,16 @@ uint8_t salsa20_expansion(uint8_t output[64], uint8_t k[32], int8_t n[16])
     const uint8_t a_3[4] = {116, 101, 32, 107};
 
     // (a_0, k_0, a_1, n, a_2, k_1, a_3)
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 4; i++)
+    {
         temp_input[i] = a_0[i];
         temp_input[i + 20] = a_1[i];
         temp_input[i + 40] = a_2[i];
         temp_input[i + 60] = a_3[i];
     }
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 16; i++)
+    {
         temp_input[i + 4] = k[i];
         temp_input[i + 24] = n[i];
         temp_input[i + 44] = k[i + 16];
@@ -208,19 +184,20 @@ uint8_t salsa20_expansion(uint8_t output[64], uint8_t k[32], int8_t n[16])
 void salsa_core_v1(uint32_t output[16], const uint32_t input[16])
 {
     // k: input[1-4] and input[11-14]
-    uint8_t k[32]; 
-    for (int i = 0; i < 4; i++) {
+    uint8_t k[32];
+    for (int i = 0; i < 4; i++)
+    {
         intToBytesP(&k[i * 4], input[i + 1]);
         intToBytesP(&k[i * 4 + 16], input[i + 11]);
     }
 
-    //nonce = input[6], input[7]
+    // nonce = input[6], input[7]
 
     uint8_t n[16];
 
     intToBytesP(n, input[6]);
     intToBytesP(&n[4], input[6]);
     // the upper 8 bytes of n remain as 0s
-    
+
     salsa20_expansion(output, k, n);
 }
