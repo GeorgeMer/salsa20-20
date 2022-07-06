52ed7242e05adabfc4615c69fe3e69351ce4ca81
David Csida <david.csida@tum.de>
2022-07-06 15:35:45 +0200

    implemented v2 with simd and without matrix transposition

--- Implementierung/salsa_core/salsa_core_v2.c
+++ Implementierung/salsa_core/salsa_core_v2.c
@@ -3,65 +3,152 @@
 
 #include "rotate_left.h"
 
-void salsa_core_v0(uint32_t output[16], const uint32_t input[16])
+void columnRound_v2(uint32_t output[16]) 
+{
+    // rotating left by 7 bits and xor'ing
+    rotateLeftSIMD_32_xor(7, 
+        output[0] +     output[12], &output[4],
+        output[5] +     output[1],  &output[9],
+        output[10] +    output[6],  &output[14],
+        output[15] +    output[11], &output[3]
+    );
+
+    // rotating left by 9 bits and xor'ing
+    rotateLeftSIMD_32_xor(9, 
+        output[0] +     output[4],  &output[8],
+        output[5] +     output[9],  &output[13],
+        output[10] +    output[14], &output[2],
+        output[15] +    output[3],  &output[7]
+    );
+
+    // rotating left by 13 bits and xor'ing
+    rotateLeftSIMD_32_xor(13, 
+        output[8] +    output[4],  &output[12],
+        output[13] +   output[9],  &output[1],
+        output[2] +    output[14], &output[6],
+        output[7] +    output[3],  &output[11]
+    );
+
+    // rotating left by 18 bits and xor'ing
+    rotateLeftSIMD_32_xor(18, 
+        output[12] +   output[8],  &output[0],
+        output[1] +    output[13], &output[5],
+        output[6] +    output[2],  &output[10],
+        output[11] +   output[7],  &output[15]
+    );
+}
+
+void rowRound_v2(uint32_t output[16]) 
+{
+    // rotating left by 7 bits and xor'ing
+    rotateLeftSIMD_32_xor(7, 
+        output[0] +     output[3], &output[1],
+        output[5] +     output[4],  &output[6],
+        output[10] +    output[9],  &output[11],
+        output[15] +    output[14], &output[12]
+    );
+
+    // rotating left by 9 bits and xor'ing
+    rotateLeftSIMD_32_xor(9, 
+        output[0] +     output[1],  &output[2],
+        output[5] +     output[6],  &output[7],
+        output[10] +    output[11], &output[8],
+        output[15] +    output[12], &output[13]
+    );
+
+    // rotating left by 13 bits and xor'ing
+    rotateLeftSIMD_32_xor(13, 
+        output[2] +    output[1],  &output[3],
+        output[7] +   output[6],  &output[4],
+        output[8] +    output[11], &output[9],
+        output[13] +    output[12],  &output[14]
+    );
+
+    // rotating left by 18 bits and xor'ing
+    rotateLeftSIMD_32_xor(18, 
+        output[3] +   output[2],  &output[0],
+        output[4] +    output[7], &output[5],
+        output[9] +    output[8],  &output[10],
+        output[14] +   output[13],  &output[15]
+    );
+
+}
+
+void salsa_core_v2(uint32_t output[16], const uint32_t input[16])
 {
     for (uint8_t i = 0; i < 16; i++)
     {
         output[i] = input[i];
     }
 
-    // 20 rounds
-    for (size_t i = 0; i < 20; i++)
+    for (size_t i = 0; i < 10; i++) 
     {
-        // rotating left by 7 bits
-        output[4] = (rotateLeft((output[0] + output[12]), 7)) ^ output[4];
-        output[9] = (rotateLeft((output[5] + output[1]), 7)) ^ output[9];
-        output[14] = (rotateLeft((output[10] + output[6]), 7)) ^ output[14];
-        output[3] = (rotateLeft((output[15] + output[11]), 7)) ^ output[3];
+        /* columnRound_v2(output);
+        rowRound_v2(output); */
+        // rotating left by 7 bits and xor'ing
+    rotateLeftSIMD_32_xor(7, 
+        output[0] +     output[12], &output[4],
+        output[5] +     output[1],  &output[9],
+        output[10] +    output[6],  &output[14],
+        output[15] +    output[11], &output[3]
+    );
 
-        // rotating left by 9 bits
-        output[8] = (rotateLeft((output[0] + output[4]), 9)) ^ output[8];
-        output[13] = (rotateLeft((output[5] + output[9]), 9)) ^ output[13];
-        output[2] = (rotateLeft((output[10] + output[14]), 9)) ^ output[2];
-        output[7] = (rotateLeft((output[15] + output[3]), 9)) ^ output[7];
+    // rotating left by 9 bits and xor'ing
+    rotateLeftSIMD_32_xor(9, 
+        output[0] +     output[4],  &output[8],
+        output[5] +     output[9],  &output[13],
+        output[10] +    output[14], &output[2],
+        output[15] +    output[3],  &output[7]
+    );
 
-        // rotating left by 13 bits
-        output[12] = (rotateLeft((output[8] + output[4]), 13)) ^ output[12];
-        output[1] = (rotateLeft((output[13] + output[9]), 13)) ^ output[1];
-        output[6] = (rotateLeft((output[2] + output[14]), 13)) ^ output[6];
-        output[11] = (rotateLeft((output[7] + output[3]), 13)) ^ output[11];
+    // rotating left by 13 bits and xor'ing
+    rotateLeftSIMD_32_xor(13, 
+        output[8] +    output[4],  &output[12],
+        output[13] +   output[9],  &output[1],
+        output[2] +    output[14], &output[6],
+        output[7] +    output[3],  &output[11]
+    );
 
-        // rotating left by 18 bits
-        output[0] = (rotateLeft((output[12] + output[8]), 18)) ^ output[0];
-        output[5] = (rotateLeft((output[1] + output[13]), 18)) ^ output[5];
-        output[10] = (rotateLeft((output[6] + output[2]), 18)) ^ output[10];
-        output[15] = (rotateLeft((output[11] + output[7]), 18)) ^ output[15];
+    // rotating left by 18 bits and xor'ing
+    rotateLeftSIMD_32_xor(18, 
+        output[12] +   output[8],  &output[0],
+        output[1] +    output[13], &output[5],
+        output[6] +    output[2],  &output[10],
+        output[11] +   output[7],  &output[15]
+    );
 
-        // transpose matrix
-        uint32_t tmp;
-        tmp = output[1];
-        output[1] = output[4];
-        output[4] = tmp;
+    // rotating left by 7 bits and xor'ing
+    rotateLeftSIMD_32_xor(7, 
+        output[0] +     output[3], &output[1],
+        output[5] +     output[4],  &output[6],
+        output[10] +    output[9],  &output[11],
+        output[15] +    output[14], &output[12]
+    );
 
-        tmp = output[2];
-        output[2] = output[8];
-        output[8] = tmp;
+    // rotating left by 9 bits and xor'ing
+    rotateLeftSIMD_32_xor(9, 
+        output[0] +     output[1],  &output[2],
+        output[5] +     output[6],  &output[7],
+        output[10] +    output[11], &output[8],
+        output[15] +    output[12], &output[13]
+    );
 
-        tmp = output[3];
-        output[3] = output[12];
-        output[12] = tmp;
+    // rotating left by 13 bits and xor'ing
+    rotateLeftSIMD_32_xor(13, 
+        output[2] +    output[1],  &output[3],
+        output[7] +   output[6],  &output[4],
+        output[8] +    output[11], &output[9],
+        output[13] +    output[12],  &output[14]
+    );
 
-        tmp = output[6];
-        output[6] = output[9];
-        output[9] = tmp;
+    // rotating left by 18 bits and xor'ing
+    rotateLeftSIMD_32_xor(18, 
+        output[3] +   output[2],  &output[0],
+        output[4] +    output[7], &output[5],
+        output[9] +    output[8],  &output[10],
+        output[14] +   output[13],  &output[15]
+    );
 
-        tmp = output[7];
-        output[7] = output[13];
-        output[13] = tmp;
-
-        tmp = output[11];
-        output[11] = output[14];
-        output[14] = tmp;
     }
 
     for (int i = 0; i < 16; i++)
--- Implementierung/testing/rotateLeft/rotateLeft_test.c
+++ Implementierung/testing/rotateLeft/rotateLeft_test.c
@@ -4,11 +4,12 @@
 #include <immintrin.h>
 #include <emmintrin.h>
 #include <string.h>
+#include <time.h>
 #include "../../salsa_core/rotate_left.h"
 
 void print128_num(__m128i var)
 {
-    uint32_t val[8];
+    uint32_t val[4];
     memcpy(val, &var, sizeof(val));
     printf("Numerical: %i %i %i %i \n",
            val[0], val[1], val[2], val[3]);
@@ -16,27 +17,50 @@ void print128_num(__m128i var)
 
 int main()
 {
+
+    clock_t start, end;
+    double cpu_time_used;
+
     uint32_t input[] = {0x02, 0x02, 0x22, 0x02};
     uint32_t output[4];
 
-
     printf("SISD:\n");
-    for (int i = 0; i < 4; i++)
+
+    start = clock();
+
+    for (int i = 0; i < 100000000; i++)
     {
-        printf("0x%x\n", rotateLeft(input[i], 4));
+        for (int i = 0; i < 4; i++)
+        {
+            /* printf("0x%x\n", rotateLeft(input[i], 4)); */
+            rotateLeft(input[i], 4);
+        }
     }
 
+    end = clock();
+
+    cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time: %lf s\n", cpu_time_used);
     printf("--------------------\n");
 
     printf("SIMD 128:\n");
-    __m128i_u _output = rotateLeftSIMD_128(
-        _mm_set_epi32(input[3], input[2], input[1], input[0]), 4);
-    /* print128_num(_output); */
 
-    _mm_storeu_si128((__m128i*) output, _output);
+    __m128i_u _output;
 
-    for (int i = 0; i < 4; i++)
-        printf("0x%x\n", output[i]);
+    start = clock();
+    for (int i = 0; i < 100000000; i++)
+    {
+        _output = rotateLeftSIMD_128(
+            _mm_set_epi32(input[3], input[2], input[1], input[0]), 4);
+    }
+    end = clock();
+
+cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Time: %lf s\n", cpu_time_used);
+    _mm_storeu_si128((__m128i *)output, _output);
+
+    /* for (int i = 0; i < 4; i++)
+        printf("0x%x\n", output[i]); */
 
     printf("--------------------\n");
 
