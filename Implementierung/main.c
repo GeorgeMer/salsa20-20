032211d3b3c72edb28816664236fcd0a8d787cfb
George <ge58bep@mytum.de>
2022-07-13 11:58:06 +0200

    Added option for decryption, wip

--- Implementierung/main.c
+++ Implementierung/main.c
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
     const char *output_file = NULL;
 
     // option parsing
-    while ((opt = getopt_long(argc, argv, "V:B:T:k:i:o:h", long_options, NULL)) != -1)
+    while ((opt = getopt_long(argc, argv, "V:B:T:k:i:o:eh", long_options, NULL)) != -1)
     {
         switch (opt)
         {
@@ -117,6 +117,9 @@ int main(int argc, char **argv)
             break;
         case 'o':
             output_file = optarg;
+            break;
+        case 'e':
+            // TODO: arrange input to be hex and output to be char
             break;
         case 'h':
             print_help(progname);
