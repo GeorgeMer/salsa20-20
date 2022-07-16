0ecd804cd6a35a69b7715b75c8dfee8a97bb9425
David Csida <david.csida@tum.de>
2022-07-16 16:24:42 +0200

    removed unused rotateLeft functions

--- Implementierung/salsa_core/rotate_left.c
+++ Implementierung/salsa_core/rotate_left.c
@@ -9,26 +9,6 @@ uint32_t rotateLeft(uint32_t number, uint32_t amount)
     return (number << amount) | (number >> (32 - amount));
 }
 
-__m128i rotateLeftSIMD_128(__m128i numbers, uint32_t amount)
-{
-    // input has to be reversed afterwards
-    return _mm_or_si128(
-        _mm_slli_epi32(numbers, amount),
-        _mm_srli_epi32(numbers, 32 - amount));
-}
-
-void rotateLeftSIMD_32(uint32_t numbers[4], uint32_t amount)
-{
-    // numbers are put into _numbers in reversed order so that they will be returned in the right order
-    __m128i_u _numbers = _mm_setr_epi32(numbers[0], numbers[2], numbers[2], numbers[3]);
-
-    _numbers = _mm_or_si128(
-        _mm_srli_epi32(_numbers, 32 - amount),
-        _mm_slli_epi32(_numbers, amount));
-
-    _mm_storeu_si128((__m128i *)numbers, _numbers);
-}
-
 void rotateLeftSIMD_32_xor(uint32_t amount, uint32_t n0, uint32_t *output0, uint32_t n1, uint32_t *output1,
                            uint32_t n2, uint32_t *output2, uint32_t n3, uint32_t *output3)
 {
@@ -50,13 +30,4 @@ void rotateLeftSIMD_32_xor(uint32_t amount, uint32_t n0, uint32_t *output0, uint
     *output1 = temp[1];
     *output2 = temp[2];
     *output3 = temp[3];
-}
-
-__m128i rotateLeftSIMD_128_xor(uint32_t amount, __m128i numbers0, __m128i numbers1, __m128i xor)
-{
-    __m128i numbers = _mm_add_epi32(numbers0, numbers1);
-    // xor has to be reversed afterwards
-    return _mm_xor_si128(_mm_or_si128(
-        _mm_slli_epi32(numbers, amount),
-        _mm_srli_epi32(numbers, 32 - amount)), xor);
 }
