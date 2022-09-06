98d958aacaea328ea9b8b2ed33460be463c7e215
George <ge58bep@mytum.de>
2022-09-06 10:16:42 +0200

    Updated comments and help message

--- Implementierung/file_IO/file_IO.c
+++ Implementierung/file_IO/file_IO.c
@@ -9,16 +9,18 @@ uint8_t *read_file(const char *path)
     // error handling
     if (file == NULL)
     {
-        perror("Error opening file");
+        perror("Error opening file\n");
         exit(EXIT_FAILURE);
     }
 
+    // find end of file
     if (fseek(file, 0, SEEK_END))
     {
         fprintf(stderr, "Error finding end of file\n");
         goto error;
     }
 
+    // find file length
     long int a = ftell(file);
     if (a == -1)
     {
@@ -26,14 +28,18 @@ uint8_t *read_file(const char *path)
         goto error;
     }
     mlen = a;
+
+    // set file pointer to file start
     rewind(file);
 
+    // allocate message
     if (!(message = malloc(mlen * sizeof(char))))
     {
         fprintf(stderr, "Error reading file: Could not allocate enough memory\n");
         goto error;
     }
 
+    // read file
     if (fread(message, mlen, 1, file) != 1)
     {
         perror("Error reading file");
@@ -65,6 +71,8 @@ void write_file(const char *path, uint8_t *cipher, int input_perms)
             perror("Error creating file");
             exit(EXIT_FAILURE);
         }
+
+        // set file permissions same as given by input_perms
         if (chmod("encrypted.bin", input_perms) < 0)
         {
 
@@ -74,11 +82,14 @@ void write_file(const char *path, uint8_t *cipher, int input_perms)
     }
     else
     {
+        // create new binary file as given by path
         if (!(file = fopen(path, "wb")))
         {
             perror("Error opening output file");
             exit(EXIT_FAILURE);
         }
+
+        // set file permissions same as given by input_perms
         if (chmod(path, input_perms) < 0)
         {
 
@@ -87,6 +98,7 @@ void write_file(const char *path, uint8_t *cipher, int input_perms)
         }
     }
 
+    // write cipher to file
     if (fwrite(cipher, mlen, 1, file) != 1)
     {
         fprintf(stderr, "Error writing to output file\n");
--- Implementierung/testing/input_tests.c
+++ Implementierung/testing/input_tests.c
@@ -14,6 +14,7 @@
 
 bool input_test(uint64_t implementation_number, const char *input_path)
 {
+    // get input file permissions
     struct stat fs;
     int r, input_perms = 384; // === 0600
 
@@ -30,6 +31,8 @@ bool input_test(uint64_t implementation_number, const char *input_path)
     uint8_t *msg;
     uint32_t key[8];
     uint64_t iv;
+
+    // set output path used for testing
     const char *output_path = "testing/io_binfiles/output.bin";
 
     // get input
@@ -39,6 +42,7 @@ bool input_test(uint64_t implementation_number, const char *input_path)
     srand(mlen + msg[mlen - 1] + time(NULL));
     randomKey(key);
     iv = randomNonce();
+
     uint8_t cipher[mlen];
 
     // run crypt
@@ -54,12 +58,12 @@ bool input_test(uint64_t implementation_number, const char *input_path)
         salsa20_crypt(mlen, msg, cipher, key, iv);
         break;
     }
-    free(msg);
+    free(msg); // variable will be used again
 
     // write to output
     write_file(output_path, cipher, input_perms);
 
-    // read output
+    // read output as input for second crypt function run
     msg = read_file(output_path);
 
     // run crypt
@@ -78,23 +82,27 @@ bool input_test(uint64_t implementation_number, const char *input_path)
     // write to output
     write_file(output_path, cipher, input_perms);
 
+    // expected and actual byte arrays representing binary of files
     uint8_t *expected, *actual;
-    // read input
+
+    // read and print input
     expected = read_file(input_path);
     size_t expLen = mlen;
     printf("Input: ");
     print_arr_8bit(expLen, expected);
     printf("\n");
 
-    // read output
+    // read and print output
     actual = read_file(output_path);
     size_t actLen = mlen;
     printf("Output after double encryption: ");
     print_arr_8bit(actLen, actual);
     printf("\n");
 
+    // get input and output permissions
     int exp_perms, act_perms;
 
+    // input perms
     r = stat(input_path, &fs);
     if (r == -1)
     {
@@ -106,6 +114,7 @@ bool input_test(uint64_t implementation_number, const char *input_path)
         exp_perms = fs.st_mode;
     }
 
+    // output perms
     r = stat(output_path, &fs);
     if (r == -1)
     {
@@ -117,13 +126,17 @@ bool input_test(uint64_t implementation_number, const char *input_path)
         act_perms = fs.st_mode;
     }
 
+    // compare permissions
     if (exp_perms != act_perms)
     {
         printf("Permissions of input and output file don't match: %d != %d\n", exp_perms, act_perms);
+        return false;
     }
 
-    // compare
+    // compare byte arrays
     bool ret = (assertEqualsArrays_8bit(expLen, expected, actLen, actual));
+
+    // deallocate and return
     free(expected);
     free(actual);
     free(msg);
@@ -132,7 +145,10 @@ bool input_test(uint64_t implementation_number, const char *input_path)
 
 bool random_input_test(uint64_t implementation_number)
 {
+    // a set input path
     const char *input_path = "testing/io_binfiles/input.bin";
+
+    // default permissions
     int input_perms = 384; // === 0600
 
     // generate random values
@@ -140,22 +156,24 @@ bool random_input_test(uint64_t implementation_number)
     uint8_t msg[mlen];
     randomMsg(mlen, msg);
 
-    // write to input
+    // write random message to to input
     write_file(input_path, msg, input_perms);
+
+    // execute test
     return input_test(implementation_number, input_path);
 }
 
-void input_tests(uint64_t implementation_number, uint64_t random_tests, const char *input_path) // input,output paths
+void input_tests(uint64_t implementation_number, uint64_t random_tests, const char *input_path)
 {
-    printf("\n\n-- Input tests: ---\n\n\n -- Manual Test:\n\n");
+    printf("\n\n-- Input tests: ---\n\n\n - Manual Test 1:\n\n");
+
+    // run manual test
     if (!input_test(implementation_number, input_path))
     {
         exit(EXIT_FAILURE);
     }
-    if (random_tests)
-    {
-        printf("-- Random Tests: --\n\n");
-    }
+
+    // run random_tests number of random tests
     for (uint64_t i = 0; i < random_tests; i++)
     {
         printf("- Random Test %lu:\n\n", i);
